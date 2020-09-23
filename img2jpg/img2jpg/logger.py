e93f4f64147394ab705e950dce21d9412d09aa4d
Nina Jensen <jnik@aarhus.dk>
2020-09-23 15:25:11 +0200

    Images2PDF version 1.2.0 (#13)
    * Version 1.1.0
    
    Fixes output path bug
    
    * Version 1.2.0: Now with multiprocessing!
    
    * Remove unused imports

---
+++ img2jpg/img2jpg/logger.py
@@ -0,0 +1,23 @@
+# -----------------------------------------------------------------------------
+# Imports
+# -----------------------------------------------------------------------------
+
+import logging
+from pathlib import Path
+
+# -----------------------------------------------------------------------------
+# Logger
+# -----------------------------------------------------------------------------
+
+
+def log_setup(log_file: Path, mode: str = "w") -> logging.Logger:
+    logger = logging.getLogger("Images2PDF")
+    file_handler = logging.FileHandler(log_file, mode)
+    file_handler.setFormatter(
+        logging.Formatter(
+            fmt="%(asctime)s %(levelname)s: %(message)s", datefmt="%H:%M:%S"
+        )
+    )
+    logger.addHandler(file_handler)
+    logger.setLevel(logging.INFO)
+    return logger
