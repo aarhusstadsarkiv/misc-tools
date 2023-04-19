842ec27bb2e9b1dc7c27ebfc21deac84de522f74
Kristian Pindstrup <56304784+kpind-aarhus@users.noreply.github.com>
2023-04-19 16:01:19 +0200

    Create functionalDescription.py

---
+++ functionalDescription/functionalDescription.py
@@ -0,0 +1,33 @@
+import xml.etree.ElementTree as ET
+from pathlib import Path
+
+# Load the XML file with the namespace map
+xml_path = Path(r'D:\test\tableIndex.xml')
+ET.register_namespace('',"http://www.sa.dk/xmlns/diark/1.0")
+tree = ET.parse(xml_path)
+root = tree.getroot()
+
+# Define the list of conditions
+conditions = [
+    {"name": "DokBeskrivelse", "functionalDescription": "Dokumenttitel"},
+    {"name": "DokDato", "functionalDescription": "Dokumentdato"},
+    {"name": "SagsTitel", "functionalDescription": "Sagstitel"},
+    {"name": "SagsNr", "functionalDescription": "Sagsidentifikation"},
+    {"name": "CICSSAGSNR", "functionalDescription": "Sagsidentifikation"},
+    {"name": "DAOSSagsnr", "functionalDescription": "Sagsidentifikation"}
+]
+
+# Find the columns that match the conditions and add the functionalDescription tag
+for column in root.findall('.//{*}column'):
+    if column.find('{*}name') is not None:
+        name = column.find('{*}name')
+    for cond in conditions:
+        #print(cond)
+        if (name.text == cond["name"] and not column.findall("{*}functionalDescription")):
+            new_tag = ET.SubElement(column, "functionalDescription")
+            #print(ET.tostring(new_tag.text, encoding='UTF-8', method="xml").decode('UTF-8'))
+            new_tag.text = cond["functionalDescription"]
+
+    # Save the modified XML file
+    save_path = xml_path.parent.joinpath(f"new_{xml_path.name}")
+    tree.write(save_path, encoding="UTF-8", xml_declaration=True, method="xml")
